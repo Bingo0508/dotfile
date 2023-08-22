6ee48fcd5f87b11adef0c085b10362376c5e51a3
manhtrancoder <139039467+manhtrancoder@users.noreply.github.com>
2023-08-22 21:19:07 +0700

    Update Microsoft.PowerShell_profile.ps1

--- Windows/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Windows/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,3 @@
-(@(& 'C:/Users/manht/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe' init powershell --config='C:\Users\manht\AppData\Local\Programs\oh-my-posh\themes\1_shell.omp.json' --print) -join "`n") | Invoke-Expression
-
 Import-Module "~\OneDrive\Documents\WindowsPowerShell\alias.ps1"	
 Import-Module PSReadLine 
 Set-PSReadLineOption -PredictionSource History
