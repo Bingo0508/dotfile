7ac5b6ce8303a5cfc3468e5037f40883f6f86655
manhtrancoder <139039467+manhtrancoder@users.noreply.github.com>
2023-09-01 14:57:55 +0700

    Update Microsoft.PowerShell_profile.ps1

--- Windows/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Windows/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
 Import-Module "~\OneDrive\Documents\WindowsPowerShell\alias.ps1"	
 Import-Module PSReadLine 
 Set-PSReadLineOption -PredictionSource History
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
